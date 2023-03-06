666123622df6c03b5688f8d09f06b90c0e1d9e27
pawanjaatpk <118508835+pawanjaatpk@users.noreply.github.com>
2023-03-06 08:23:30 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/lazydeveloperr/MdiskSearchBOTTC.git /MdiskSearchBOTTC
+git clone https://github.com/pawanjaatpk/MdiskSearchBOTTC.git /MdiskSearchBOTTC
 cd /MdiskSearchBOTTC
 pip3 install -r requirements.txt
 echo "Starting Bot...."
